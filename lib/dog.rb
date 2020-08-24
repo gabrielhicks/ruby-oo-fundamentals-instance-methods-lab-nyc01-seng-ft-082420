d3fdda3cc12af1382967ce45f996219ccef7225d
gabrielhicks <gabrielhicks@mail.com>
2020-08-24 17:06:49 -0400

    we added instances to our dog and person classes

---
+++ lib/dog.rb
@@ -0,0 +1,12 @@
+class Dog
+
+    #adding instance bark to dog class
+    def bark
+        puts "Woof!"
+    end
+    
+    #adding instance sit
+    def sit
+        puts "The Dog is sitting"
+    end
+end
---
+++ lib/person.rb
@@ -0,0 +1,10 @@
+class Person
+    #adding talk instance
+    def talk
+        puts "Hello World!"
+    end
+    #defining walk
+    def walk
+        puts "The Person is walking"
+    end
+end
